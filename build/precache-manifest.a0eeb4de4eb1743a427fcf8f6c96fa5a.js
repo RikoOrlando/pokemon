7e2e926b8099911fc1b3fc2aa7cf172104205f7f
Riko Orlando <rikoorlando76@gmail.com>
2020-06-22 10:36:35 +0700

    .

---
+++ build/precache-manifest.a0eeb4de4eb1743a427fcf8f6c96fa5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8cfde477afe84e82512bbf29345f64",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "967f222b66560500bbb9",
+    "url": "/pokemon/static/css/main.514be4de.chunk.css"
+  },
+  {
+    "revision": "ad11fee8be85b3571346",
+    "url": "/pokemon/static/js/2.06201693.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/pokemon/static/js/2.06201693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967f222b66560500bbb9",
+    "url": "/pokemon/static/js/main.a89534b4.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  },
+  {
+    "revision": "61cf7c6d8c344ffe3a31515553d0d2ba",
+    "url": "/pokemon/static/media/Search.61cf7c6d.svg"
+  },
+  {
+    "revision": "184453072f4d631c731fde9092266d17",
+    "url": "/pokemon/static/media/close.18445307.svg"
+  },
+  {
+    "revision": "3a8e4dbe6481747532acd2f0b2c5e9c1",
+    "url": "/pokemon/static/media/height.3a8e4dbe.svg"
+  },
+  {
+    "revision": "036b812a37c4345af11bdf8aa5be8cd3",
+    "url": "/pokemon/static/media/page-not-found.036b812a.svg"
+  },
+  {
+    "revision": "e867b0db0bf4e18cccd2ab72ca4e6ada",
+    "url": "/pokemon/static/media/strength.e867b0db.svg"
+  },
+  {
+    "revision": "9659255fe277facaabbf8e58b5671e42",
+    "url": "/pokemon/static/media/weight-scale.9659255f.svg"
+  }
+]);
